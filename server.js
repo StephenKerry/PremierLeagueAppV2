920bb915cd48a98fb9c1678c2ed0bf9ba3b36a7d
Glitch (glitch-hello-node) <none>
2025-01-28 11:59:31 +0000

    🌇💃 Checkpoint ./server.js:639123/227

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ import routes from './routes.js';
 const app = express();
 const port = 3000;
 
-// app.use("/", routes);
-
-app.get('/', (request, response) => response.send('This the home page'));
-app.get('/hello', (request, response) => response.send('hello!!!'));
-app.get('/test', (request, response) => response.send('This is a test page!'));
+app.use("/", routes);
 
 app.listen(port, () => logger.info("Your app is listening on port " + port));
