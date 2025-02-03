471a33d083520f7b8498ccad4a49d0da3052afe7
Glitch (topic-01-lab-rb-25) <none>
2025-02-03 17:55:15 +0000

    📚⛹ Checkpoint ./views/partials/menu.hbs:639123/168 ./views/start.hbs:639123/2638 ./server.js:639123/892 ./public/style.css:639123/237 ./public/script.js:639123/48 ./views/about.hbs:639123/329 ./controllers/about.js:639123/877

--- controllers/about.js
+++ controllers/about.js
@@ -3,8 +3,32 @@ import logger from "../utils/logger.js";
 
 const about = {
   createView(request, response) {
-    response.send('About the Playlist app');   
+    logger.info("Start page loading!");
+    
+    const viewData = {
+      title: "Welcome to the Playlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData);   
   },
 };
 
 export default about;
+
+const start = {
+  createView(request, response) {
+    logger.info("Start page loading!");
+    
+    const viewData = {
+      title: "Welcome to the Playlist app!",
+      info: appStore.getAppInfo()
+    };
+    
+    //logger.debug(viewData);
+    response.render('start', viewData);   
+  },
+};
+
+export default start;
