a5ebda00a5c79f8f5fd4b8bddb0abefdf318b4f5
Glitch (topic-01-lab-rb-25) <none>
2025-02-03 17:31:57 +0000

    👩‍❤️‍💋‍👩🏘 Checkpoint ./models/app-store.json:639123/289 ./models/app-store.js:639123/1295 ./package.json:639123/1557 ./models/json-store.js:639123/1863 ./controllers/start.js:639123/553 ./server.js:639123/694 ./views/layouts/main.hbs:639123/1033 ./views/partials/menu.hbs:639123/289

--- controllers/start.js
+++ controllers/start.js
@@ -1,12 +1,17 @@
 'use strict';
+
 import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
 const start = {
+
   createView(request, response) {
+    const info = appStore.getAppInfo();
+    logger.debug(info);
+    
     logger.info("Start page loading!")
-    response.send('Welcome to the Playlist app!');   
+    response.json(info);   
   },
 };
-
 
 export default start;
