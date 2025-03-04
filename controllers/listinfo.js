412852d82eccb6fa45b20bd71be0966d9108415a
Glitch (topic-02-lab-rb-25) <none>
2025-03-04 11:51:42 +0000

    🍠🍟 Checkpoint ./controllers/listinfo.js:74048576/193

--- controllers/listinfo.js
+++ controllers/listinfo.js
@@ -1,13 +1,15 @@
 'use strict';
 
-import logger from "../utils/logger.js";
+import logger from "../utils/logger.js"; 
+import playlistStore from '../models/playlist-store.js';
 
 const listinfo = {
   createView(request, response) {
     logger.info("Info page loading!");
     
     const viewData = {
-      title: "Team Infomation Details"
+      title: "Team Infomation Details" ,
+       singleTeam: mycollection.getAllTeams(teamsId)
     };
     
     response.render('listinfo', viewData);
