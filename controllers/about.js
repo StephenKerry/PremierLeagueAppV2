fb11934f50a2edb1e8068d5b850337b90b1811ab
Glitch (topic-02-lab-rb-25) <none>
2025-03-06 11:22:17 +0000

    🌁🐔 Checkpoint ./controllers/about.js:74048576/1314 ./public/style.css:74048576/514 ./views/layouts/main.hbs:74048576/58

--- controllers/about.js
+++ controllers/about.js
@@ -5,22 +5,23 @@ import logger from "../utils/logger.js";
 const about = {
   createView(request, response) {
     logger.info("About page loading!");
+
     
     const viewData = {
-      title: "Premier League App About" ,
-      info:{"info": {
-    "appTitle": "Premier League App",
-    "version": 0.2,
-    "creators": [
-      {
-        "name": "Stephen Kerry",
-        "role": "Web Developer"  }
-      
-    ],
-    "location": "Waterford"
-  } 
-}
+      title: "Premier League App About",
+      info: {
+        appTitle: "Premier League App",
+        version: 0.2,
+        creators: [
+          {
+            name: "Stephen Kerry",
+            role: "Web Developer"
+          }
+        ],
+        location: "Waterford"
+      }
     };
+
     
     response.render('about', viewData);
   },
