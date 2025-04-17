c9dcfa7f27de482fc14de3d56d3c92165771e553
Glitch (topic-02-lab-rb-25) <none>
2025-04-17 07:50:19 +0000

    📒💖 Checkpoint ./views/partials/welcomemenu.hbs:74048576/557 ./public/style.css:74048576/743 ./views/partials/menu.hbs:74048576/116 ./controllers/accounts.js:74048576/1837 ./routes.js:74048576/382

--- routes.js
+++ routes.js
@@ -12,8 +12,9 @@ import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js';
 import listinfo from './controllers/listinfo.js'; 
 import search from './controllers/search.js';
+import accounts from './controllers/accounts.js';
 router.get('/search', search.createView);
-router.get('/', start.createView);
+router.get('/start', start.createView);
 router.get('/dashboard', dashboard.createView); 
 router.get('/about', about.createView); 
 router.get('/team/:id', listinfo.createView);  
@@ -24,7 +25,14 @@ router.get('/dashboard/deleteteam/:id', dashboard.deleteTeam);
 router.post('/searchCategory', search.findResult);
 router.post('/dashboard/addteam', dashboard.addTeam);
 router.get('/search', search.createView);  // Show the search page
-router.post('/search', search.findResult);  // Handle form submission and show search results
+router.post('/search', search.findResult); // Handle form submission and show search results
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
+
 
 
 
