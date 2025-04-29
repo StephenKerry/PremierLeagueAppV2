50ca42d52a2ee634eefbca59065301d94834c3d0
Glitch (topic-02-lab-rb-25) <none>
2025-04-29 07:47:09 +0000

    ⛈👼 Checkpoint ./models/user-store.js:74048576/1566

--- models/user-store.js
+++ models/user-store.js
@@ -4,26 +4,38 @@ import logger from '../utils/logger.js';
 import JsonStore from './json-store.js';
 
 const userStore = {
-
   store: new JsonStore('./models/user-store.json', { users: [] }),
   collection: 'users',
 
   getAllUsers() {
     return this.store.findAll(this.collection);
   },
-  
+
   getUserById(id) {
-    return this.store.findOneBy(this.collection, (user => user.id === id));
-  },
-  
-  getUserByEmail(email) {
-    return this.store.findOneBy(this.collection, (user => user.email === email));
-  },
-  
-  addUser(user) {
-    this.store.addCollection(this.collection, user);
+    return this.store.findOneBy(this.collection, (user) => user.id === id);
   },
 
+  getUserByEmail(email) {
+    return this.store.findOneBy(this.collection, (user) => user.email === email);
+  },
+
+  async addUser(user, picture) {
+    try {
+      
+      const imageUrl = await this.store.uploader({ picture });
+
+      
+      user.picture = imageUrl;
+
+      
+      await this.store.addCollection(this.collection, user);
+
+      logger.info(`User ${user.email} registered successfully`);
+    } catch (err) {
+      logger.error('Failed to register user with image:', err);
+      throw new Error('Registration failed');
+    }
+  },
 };
 
 export default userStore;
